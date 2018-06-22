96ef99fea153d33d5827248e457d6aabab07d270
Glitch (rchain-bounty) <none>
2018-06-22 16:10:55 +0000

    👫👢 Updated with Glitch

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -16,7 +16,34 @@ function hbsHelpers(hbs) {
         //return date;
         return ""+date.replace("T", " ").substring(0,16);
         //return date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear()
+    },
+      ifCond: function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
     }
+}
       // More helpers...
     }
 
